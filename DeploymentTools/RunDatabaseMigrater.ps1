821a2997d66c9cbc94371ec2a11ebd13edad14b4
Rhian <rhian.gwawr@gmail.com>
2019-08-04 13:31:11 +0100

    Corrected database migrater PowerShell

--- DeploymentTools/RunDatabaseMigrater.ps1
+++ DeploymentTools/RunDatabaseMigrater.ps1
@@ -1,3 +1,3 @@
 param($MigraterPath, $ConnectionString, $DatabaseName, $WebAppUser, $ServiceUser, $DomainName)
 
-Start-Process -FilePath $MigraterPath -ArgumentList $ConnectionString, $DatabaseName, $WebAppUser, $ServiceUser, $DomainName
+Start-Process -FilePath $MigraterPath -ArgumentList ("`"" + $ConnectionString + "`" `"" + $DatabaseName + "`" `"" + $WebAppUser + "`" `"" + $ServiceUser + "`" `"" + $DomainName + "`"")
