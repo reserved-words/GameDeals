bdf2173b9023dc00ecc795924910056003ace034
Rhian <rhian.gwawr@gmail.com>
2019-08-26 13:37:45 +0100

    More corrections to CreateUsers SQL

--- DatabaseMigrater/CreateUsers.sql
+++ DatabaseMigrater/CreateUsers.sql
@@ -1,24 +1,23 @@
-﻿DECLARE @SqlStatement NVARCHAR(500)
+﻿-- Create Service User
 
-IF NOT EXISTS (SELECT [LoginName] FROM SYSLOGINS WHERE [Name] = '$(WebAppUser)' AND DBNAME = '$(DatabaseName)')
-BEGIN
-    SET @SqlStatement = 'CREATE LOGIN ' + QUOTENAME('$(WebAppUser)') + ' FROM WINDOWS WITH DEFAULT_DATABASE=[$(DatabaseName)], DEFAULT_LANGUAGE=[us_english]'
-    EXEC sp_executesql @SqlStatement
-END
-
-IF NOT EXISTS (SELECT [Name] FROM SYSUSERS WHERE [Name] = '$(WebAppUser)')
-BEGIN
-    CREATE USER [$(WebAppUser)] FOR LOGIN [$(WebAppUser)] WITH DEFAULT_SCHEMA = GameDeals
-END
-
-GRANT CONNECT TO [$(WebAppUser)]
-GRANT SELECT ON SCHEMA::GameDeals TO [$(WebAppUser)]
+DECLARE @SqlStatement NVARCHAR(500)
 
 IF NOT EXISTS (SELECT LoginName FROM SYSLOGINS WHERE NAME = '$(ServiceUser)' AND DBNAME = '$(DatabaseName)')
 BEGIN
     SET @SqlStatement = 'CREATE LOGIN ' + QUOTENAME('$(ServiceUser)') + ' FROM WINDOWS WITH DEFAULT_DATABASE=[$(DatabaseName)], DEFAULT_LANGUAGE=[us_english]'
     EXEC sp_executesql @SqlStatement
 END
+
+IF NOT EXISTS (SELECT [Name] FROM SYSUSERS WHERE [Name] = '$(ServiceUser)')
+BEGIN
+    CREATE USER [$(ServiceUser)] FOR LOGIN [$(ServiceUser)] WITH DEFAULT_SCHEMA = GameDeals
+END
+
+GRANT CONNECT TO [$(ServiceUser)]
+GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::GameDeals TO [$(ServiceUser)]
+
+
+-- Create App Pool User
 
 IF NOT EXISTS (SELECT [Name] FROM SYSUSERS WHERE [Name] = '$(WebAppUser)')
 BEGIN
@@ -28,11 +27,3 @@ END
 GRANT CONNECT TO [$(WebAppUser)]
 GRANT SELECT ON SCHEMA::GameDeals TO [$(WebAppUser)]
 GRANT UPDATE ON GameDeals.Posts TO [$(WebAppUser)]
-
-IF NOT EXISTS (SELECT [Name] FROM SYSUSERS WHERE [Name] = '$(ServiceUser)')
-BEGIN
-    CREATE USER [$(ServiceUser)] FOR LOGIN [$(ServiceUser)] WITH DEFAULT_SCHEMA = GameDeals
-END
-
-GRANT CONNECT TO [$(ServiceUser)]
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::GameDeals TO [$(ServiceUser)]
