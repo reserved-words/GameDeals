781342dd4f76183a4c894d3477f3f5d98e83b2d7
Rhian Adams <rhian.gwawr@gmail.com>
2019-10-23 19:58:36 +0100

    Moved config from JS to appsettings file

--- GameDeals.UI/GameDeals/Scripts/auth.js
+++ GameDeals.UI/GameDeals/Scripts/auth.js
@@ -1,46 +1,31 @@
-﻿//function log() {
-//    document.getElementById('results').innerText = '';
+﻿var config = "";
+var mgr = null;
 
-//    Array.prototype.forEach.call(arguments, function (msg) {
-//        if (msg instanceof Error) {
-//            msg = "Error: " + msg.message;
-//        }
-//        else if (typeof msg !== 'string') {
-//            msg = JSON.stringify(msg, null, 2);
-//        }
-//        document.getElementById('results').innerHTML += msg + '\r\n';
-//    });
-//}
+function authorize(config){
+    var authConfig = {
+        authority: config.authUrl,
+        client_id: config.authClientId,
+        redirect_uri: config.authCallback,
+        response_type: config.authResponseType,
+        scope: config.authScope,
+        post_logout_redirect_uri: config.homeUrl,
+    };
 
-$("body").on("click", "#login", login);
+    mgr = new Oidc.UserManager(authConfig);
 
-//document.getElementById("login").addEventListener("click", login, false);
-//document.getElementById("api").addEventListener("click", api, false);
-//document.getElementById("logout").addEventListener("click", logout, false);
-
-var config = {
-    authority: "http://localhost:5000",
-    client_id: "GameDeals",
-    redirect_uri: "http://localhost:50606/Callback",
-    response_type: "code",
-    scope: "GameDealsApi",
-    post_logout_redirect_uri: "http://localhost:50606/",
-};
-
-var mgr = new Oidc.UserManager(config);
-
-mgr.getUser().then(function (user) {
-    if (user) {
-        console.log("User logged in");
-        loadCategories(true);
-    }
-    else {
-        console.log("User not logged in");
-        ko.applyBindings(mainViewModel);
-        mainViewModel.loading(false);
-        loadContent(null, "http://localhost:50606/Login", null);
-    }
-});
+    mgr.getUser().then(function (user) {
+        if (user) {
+            console.log("User logged in");
+            loadCategories(true);
+        }
+        else {
+            console.log("User not logged in");
+            ko.applyBindings(mainViewModel);
+            mainViewModel.loading(false);
+            loadContent(null, config.loginUrl, null);
+        }
+    });
+}
 
 function login() {
     mgr.signinRedirect();
@@ -48,26 +33,29 @@ function login() {
 
 function callback() {
     new Oidc.UserManager({ response_mode: "query" }).signinRedirectCallback().then(function () {
-        window.location = "http://localhost:50606/";
+        window.location = config.homeUrl;
     }).catch(function (e) {
         console.error(e);
     });
 }
 
-//function api() {
-//    mgr.getUser().then(function (user) {
-//        var url = "http://localhost:5001/identity";
-
-//        var xhr = new XMLHttpRequest();
-//        xhr.open("GET", url);
-//        xhr.onload = function () {
-//            log(xhr.status, JSON.parse(xhr.responseText));
-//        }
-//        xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
-//        xhr.send();
-//    });
-//}
-
 function logout() {
     mgr.signoutRedirect();
 }
+
+$(function ()
+{
+    $("body").on("click", "#login", login);
+
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "appsettings.json", false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                config = JSON.parse(rawFile.responseText);
+                authorize(config);
+            }
+        }
+    }
+    rawFile.send(null);
+});
