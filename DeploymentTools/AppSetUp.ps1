3bdff175a57ba4d8d531a3e52f3d5a3c2626465f
Rhian Adams <rhian.gwawr@gmail.com>
2019-11-14 17:27:46 +0000

    Added setup for API app

--- DeploymentTools/AppSetUp.ps1
+++ DeploymentTools/AppSetUp.ps1
@@ -1,8 +1,8 @@
-param($DomainName, $AppName, $ServiceUser, $ServicePassword)
+param($DomainName, $AppName, $ApiName, $ServiceUser, $ServicePassword)
 
 Import-Module $PSScriptRoot\SetupTools.psm1
 
 Run-Setup -DomainName $DomainName
-# Setup-WebApp -UserName $WebAppUser -Password $WebAppPassword -AppName $AppName
 Setup-WebApp -AppName $AppName
+Setup-WebApp -AppName $ApiName
 Setup-Service -UserName $ServiceUser -Password $ServicePassword -AppName $AppName
